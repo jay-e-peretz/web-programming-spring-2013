5c29434f6b4c9fb2c9b0eb1b09fb9e4cb145d9b0
Jay Peretz <jperetz@santarosa.edu>
2013-01-23 21:34:46 -0800

    added my first php page to lesson 2

---
+++ lesson-02/index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<?php
+//  kjdkljldjlkjdkljdld
+function age($birthdate) {
+	return (strtotime('now') - strtotime($birthdate))/(60*60*24*365);
+}	
+$age =  age('1957-12-01');
+
+if ($age > 21) {
+	?>
+    <h2 style="color:green">You are of age!, Have fun, but drink responsibly.</h2>
+    
+    <?php
+	
+} else {
+	?>
+    <h2 style="color:red">Sorry, you are too young.  How about some root beer?</h2>
+    
+    <?php
+	
+}
+
+
+
+?>
+
+</body>
+</html>
